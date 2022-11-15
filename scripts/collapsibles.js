e3beb84b24c1d859e6a3aae2097ce16a04b32c60
dkp-consult <96497744+dkp-consult@users.noreply.github.com>
2022-11-15 11:46:09 +0100

    Fix the about-me collapsible section

--- scripts/collapsibles.js
+++ scripts/collapsibles.js
@@ -12,3 +12,16 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+var coll = document.getElementsByClassName("mobile");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function(){
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight =  content.scrollHeight + "px"; /* use 600 can fix the heigh of div */
+        }
+    });
+}
